fee98a2938d417502f638f3e8501d89a1e3d3642
Abhishek Kumar <abhishekrajputji2004@gmail.com>
2025-07-08 02:18:00 +0530

    Added common and backend-common

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import {WebSocketServer} from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -12,7 +12,13 @@ wss.on('connection', function connection(ws,request) {
   const queryParams = new URLSearchParams(url.split('?')[1]);
   const token = queryParams.get('token')|| "";
   const decoded = jwt.verify(token,JWT_SECRET);
-  if(!decoded || !(decoded as JwtPayload).userId){
+  if(typeof decoded == "string"){
+    ws.close();
+    return;
+  }
+
+  
+  if(!decoded || !decoded.userId){
     ws.close();
     return;
   }
