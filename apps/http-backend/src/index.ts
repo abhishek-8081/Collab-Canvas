76e297bc32de750e889268c23d7f61f97f1a6f79
Abhishek Kumar <abhishekrajputji2004@gmail.com>
2025-07-08 01:36:47 +0530

    Added Business Logic in http and ws-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,3 +1,44 @@
 import express from "express";
+// import { z } from 'zod';
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
+
 const app = express();
+app.use(express.json());
+
+
+app.post("/signup",(req,res)=>{
+    
+    res.json({
+        userId:"123"
+    })
+
+})
+
+
+app.post("/signin",(req,res)=>{
+
+
+    const userId =1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+
+    res.send({token});
+
+
+    
+})
+
+
+app.post("/room",middleware,(req,res)=>{
+    //db call
+    res.json({
+        roomId:"123"
+    })
+    
+})
+
+
 app.listen(3001);
