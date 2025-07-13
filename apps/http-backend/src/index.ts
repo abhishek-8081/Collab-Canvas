e8cfbe1bf6157a3fbc5b613e6ac3e5a9e159c13f
Abhishek Kumar <abhishekrajputji2004@gmail.com>
2025-07-13 16:01:49 +0530

    Added Zod Schema by adding common Package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,12 +3,22 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateUserSchema ,SiginUserSchema,CreateRoomSchema } from "@repo/common/types";
 
 const app = express();
 app.use(express.json());
 
 
 app.post("/signup",(req,res)=>{
+
+    const data = CreateUserSchema.safeParse(req.body);
+        if(!data.success){
+            res.json({
+                mesaage:"Incorrect Inputs"
+            })
+            return;
+        }
+    
     
     res.json({
         userId:"123"
@@ -18,6 +28,13 @@ app.post("/signup",(req,res)=>{
 
 
 app.post("/signin",(req,res)=>{
+    const data = SiginUserSchema.safeParse(req.body);
+        if(!data.success){
+            res.json({
+                mesaage:"Incorrect Inputs"
+            })
+            return;
+        }
 
 
     const userId =1;
@@ -33,6 +50,14 @@ app.post("/signin",(req,res)=>{
 
 
 app.post("/room",middleware,(req,res)=>{
+
+    const data = CreateRoomSchema.safeParse(req.body);
+        if(!data.success){
+            res.json({
+                mesaage:"Incorrect Inputs"
+            })
+            return;
+        }
     //db call
     res.json({
         roomId:"123"
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,21 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 
-export function middleware(req:Request,res:Response,next:NextFunction){
+export function middleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers["authorization"] ?? "";
-    const decoded = jwt.verify(token,JWT_SECRET);
-    if(decoded){
-        // To Do from TypeScript
-        req.userId = decodeURIComponent.userId;
-        next();
 
-    }else{
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (decoded) {
+        // @ts-ignore: TODO: Fix this
+        req.userId = decoded.userId;
+        next();
+    } else {
         res.status(403).json({
-            message:"Unauthorized Access"
+            message: "Unauthorized"
         })
     }
-
-
 }
