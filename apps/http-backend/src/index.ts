4e8cb43dbdd08c436dfec82a4bd9f5d93598d500
Abhishek Kumar <abhishekrajputji2004@gmail.com>
2025-07-14 10:53:13 +0530

    Added type:module in package.json to enable ES module syntax

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,67 +2,95 @@ import express from "express";
 // import { z } from 'zod';
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import { middleware } from "./middleware";
-import { CreateUserSchema ,SiginUserSchema,CreateRoomSchema } from "@repo/common/types";
+import { middleware } from "./middleware.js";
+import { CreateUserSchema, SiginUserSchema, CreateRoomSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
+
 
 const app = express();
 app.use(express.json());
 
 
-app.post("/signup",(req,res)=>{
+app.post("/signup", async (req, res) => {
 
-    const data = CreateUserSchema.safeParse(req.body);
-        if(!data.success){
-            res.json({
-                mesaage:"Incorrect Inputs"
-            })
-            return;
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        console.log(parsedData.error);
+        res.json({
+            mesaage: "Incorrect Inputs"
+        })
+        return;
+    }
+    try {
+        const user = await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.username,
+                //Hash the Password
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+        res.json({
+            userId: user.id
+        })
+    }
+    catch {
+        res.status(411).json({
+            message: "User Already exist with this email/username"
+        })
+    }
+})
+
+
+app.post("/signin", async(req, res) => {
+    const parsedData = SiginUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.json({
+            mesaage: "Incorrect Inputs"
+        })
+        return;
+    }
+
+    //To - do Compare the Hashed Password
+    const user = await prismaClient.user.findFirst({
+        where:{
+            email:parsedData.data.username,
+            password:parsedData.data.password
         }
-    
-    
-    res.json({
-        userId:"123"
+        
     })
+    if(!user){
+        res.status(403).json({
+            message:"Not Authorized"
+        })
+        return;
+    }
 
-})
-
-
-app.post("/signin",(req,res)=>{
-    const data = SiginUserSchema.safeParse(req.body);
-        if(!data.success){
-            res.json({
-                mesaage:"Incorrect Inputs"
-            })
-            return;
-        }
-
-
-    const userId =1;
     const token = jwt.sign({
-        userId
-    },JWT_SECRET);
+        userId:user?.id
+    }, JWT_SECRET);
 
-    res.send({token});
+    res.send({ token });
 
 
-    
+
 })
 
 
-app.post("/room",middleware,(req,res)=>{
+app.post("/room", middleware, (req, res) => {
 
     const data = CreateRoomSchema.safeParse(req.body);
-        if(!data.success){
-            res.json({
-                mesaage:"Incorrect Inputs"
-            })
-            return;
-        }
+    if (!data.success) {
+        res.json({
+            mesaage: "Incorrect Inputs"
+        })
+        return;
+    }
     //db call
     res.json({
-        roomId:"123"
+        roomId: "123"
     })
-    
+
 })
 
 
